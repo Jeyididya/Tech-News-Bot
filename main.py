bfa9d82911d11ad6385d0db2c1a5c3d378f652d8
Yididya <jedidiahamdualem@gmail.com>
2022-09-30 22:37:38 +0300

    measa

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ def main():
         url_path=TOKEN,
         webhook_url='https://aqueous-inlet-81898.herokuapp.com/' + TOKEN
     )
-
+    updater.start_polling()
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
     # start_polling() is non-blocking and will stop the bot gracefully.
